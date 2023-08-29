06a2f7633ea0da5291f1ad19ff1435c5c022a117
Henry <henryhuang861219@gmail.com>
2023-08-29 11:10:28 +0800

    chore: redesign the setting page

--- app/src/main/java/me/lazy_assedninja/android_dumpling_timer/ui/option/OptionFragment.kt
+++ app/src/main/java/me/lazy_assedninja/android_dumpling_timer/ui/option/OptionFragment.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import me.lazy_assedninja.android_dumpling_timer.R
 import me.lazy_assedninja.android_dumpling_timer.databinding.FragmentOptionBinding
-import me.lazy_assedninja.android_dumpling_timer.ui.timer.TimerViewModel
 import me.lazy_assedninja.android_dumpling_timer.ui.base.BaseFragment
+import me.lazy_assedninja.android_dumpling_timer.ui.timer.TimerViewModel
 import me.lazy_assedninja.android_dumpling_timer.util.autoCleared
 
 class OptionFragment : BaseFragment() {
@@ -31,7 +31,7 @@ class OptionFragment : BaseFragment() {
         timerViewModel // For init.
 
         binding.btTimer.setOnClickListener {
-            if (timerViewModel.setting == null) Toast.makeText(it.context, R.string.toast_set_time_first, Toast.LENGTH_SHORT).show()
+            if (timerViewModel.setting == null) Snackbar.make(binding.root, R.string.toast_set_time_first, Snackbar.LENGTH_SHORT).show()
             else findNavController().navigate(OptionFragmentDirections.toTimerFragment())
         }
         binding.btSetting.setOnClickListener {
--- app/src/main/java/me/lazy_assedninja/android_dumpling_timer/ui/setting/SettingFragment.kt
+++ app/src/main/java/me/lazy_assedninja/android_dumpling_timer/ui/setting/SettingFragment.kt
@@ -1,6 +1,8 @@
 package me.lazy_assedninja.android_dumpling_timer.ui.setting
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,12 +11,13 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 import kotlinx.coroutines.launch
 import me.lazy_assedninja.android_dumpling_timer.R
 import me.lazy_assedninja.android_dumpling_timer.databinding.FragmentSettingBinding
 import me.lazy_assedninja.android_dumpling_timer.ui.base.BaseFragment
 import me.lazy_assedninja.android_dumpling_timer.util.autoCleared
-import java.lang.Exception
 
 class SettingFragment : BaseFragment() {
 
@@ -34,36 +37,117 @@ class SettingFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
-            btFinish.setOnClickListener {
+            fun Editable?.checkNumberFormat(textInputLayout: TextInputLayout) {
                 try {
-                    if (etBaseTime.text.isEmpty() || etGapTime1.text.isEmpty() || etGapTime2.text.isEmpty() || etGapTime3.text.isEmpty() || etGapTime4.text.isEmpty() || etSoundEffectLoopTime.text.isEmpty()) {
-                        Toast.makeText(it.context, R.string.toast_set_time_first, Toast.LENGTH_SHORT).show()
-                        return@setOnClickListener
-                    }
-                    viewModel.setSetting(
-                        etBaseTime.text.toString().toLong(), listOf(
-                            etGapTime1.text.toString().toLong(),
-                            etGapTime2.text.toString().toLong(),
-                            etGapTime3.text.toString().toLong(),
-                            etGapTime4.text.toString().toLong()
-                        ), etSoundEffectLoopTime.text.toString().toInt()
-                    )
-                    requireActivity().onBackPressed()
+                    this.toString().toLong()
+                    textInputLayout.error = null
                 } catch (e: NumberFormatException) {
-                    Toast.makeText(it.context, R.string.toast_wrong_number_format, Toast.LENGTH_SHORT).show()
+                    textInputLayout.error = getString(R.string.error_wrong_time_format)
                 }
+            }
+            tieBaseTime.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    s.checkNumberFormat(tilBaseTime)
+                }
+            })
+            tieGapTime1.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    s.checkNumberFormat(tilGapTime1)
+                }
+            })
+            tieGapTime2.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    s.checkNumberFormat(tilGapTime2)
+                }
+            })
+            tieGapTime3.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    s.checkNumberFormat(tilGapTime3)
+                }
+            })
+            tieGapTime4.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    s.checkNumberFormat(tilGapTime4)
+                }
+            })
+            tieSoundEffectLoopTime.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    try {
+                        s.toString().toInt()
+                        tilSoundEffectLoopTime.error = null
+                    } catch (e: NumberFormatException) {
+                        tilSoundEffectLoopTime.error = getString(R.string.error_wrong_number_format)
+                    }
+                }
+            })
+
+            btFinish.setOnClickListener {
+                dismissKeyboard(it.windowToken)
+                if (tieBaseTime.text.isNullOrEmpty() || tieGapTime1.text.isNullOrEmpty() || tieGapTime2.text.isNullOrEmpty() || tieGapTime3.text.isNullOrEmpty() || tieGapTime4.text.isNullOrEmpty() || tieSoundEffectLoopTime.text.isNullOrEmpty()) {
+                    Snackbar.make(binding.root, R.string.toast_set_time_first, Snackbar.LENGTH_SHORT).show()
+                    return@setOnClickListener
+                }
+                if (!tilBaseTime.error.isNullOrEmpty() || !tilGapTime1.error.isNullOrEmpty() || !tilGapTime2.error.isNullOrEmpty() || !tilGapTime3.error.isNullOrEmpty() || !tilGapTime4.error.isNullOrEmpty() || !tilSoundEffectLoopTime.error.isNullOrEmpty()) {
+                    Snackbar.make(binding.root, R.string.error_wrong_time_format, Snackbar.LENGTH_SHORT).show()
+                    return@setOnClickListener
+                }
+                viewModel.setSetting(
+                    tieBaseTime.text.toString().toLong(), listOf(
+                        tieGapTime1.text.toString().toLong(),
+                        tieGapTime2.text.toString().toLong(),
+                        tieGapTime3.text.toString().toLong(),
+                        tieGapTime4.text.toString().toLong()
+                    ), tieSoundEffectLoopTime.text.toString().toInt()
+                )
+                requireActivity().onBackPressed()
             }
 
             lifecycleScope.launch {
                 viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                     viewModel.setting.collect {
                         if (it == null) return@collect
-                        etBaseTime.setText(it.baseTime.toString())
-                        etGapTime1.setText(it.gapTimeList[0].toString())
-                        etGapTime2.setText(it.gapTimeList[1].toString())
-                        etGapTime3.setText(it.gapTimeList[2].toString())
-                        etGapTime4.setText(it.gapTimeList[3].toString())
-                        etSoundEffectLoopTime.setText(it.soundEffectLoopTime.toString())
+                        tieBaseTime.setText(it.baseTime.toString())
+                        tieGapTime1.setText(it.gapTimeList[0].toString())
+                        tieGapTime2.setText(it.gapTimeList[1].toString())
+                        tieGapTime3.setText(it.gapTimeList[2].toString())
+                        tieGapTime4.setText(it.gapTimeList[3].toString())
+                        tieSoundEffectLoopTime.setText(it.soundEffectLoopTime.toString())
                     }
                 }
             }
