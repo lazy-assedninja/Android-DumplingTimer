f120b5b12f1e3e114a06bc23d19eaeb53319ee04
Henry <henryhuang861219@gmail.com>
2023-08-29 14:36:45 +0800

    chore: redesign the confirm dialog

--- app/src/main/java/me/lazy_assedninja/android_dumpling_timer/ui/timer/ConfirmRevertDialog.kt
+++ app/src/main/java/me/lazy_assedninja/android_dumpling_timer/ui/timer/ConfirmRevertDialog.kt
@@ -1,9 +1,12 @@
 package me.lazy_assedninja.android_dumpling_timer.ui.timer
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.DialogFragment
 import me.lazy_assedninja.android_dumpling_timer.databinding.DialogConfirmRevertBinding
@@ -14,6 +17,9 @@ class ConfirmRevertDialog(val revertData: () -> Unit) : DialogFragment() {
     private var binding by autoCleared<DialogConfirmRevertBinding>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog?.window?.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT)
+
         binding = DialogConfirmRevertBinding.inflate(inflater, container, false).apply {
             lifecycleOwner = viewLifecycleOwner
         }
